df36028d60980694902be44732c458fbf4f65983
Felipe <rimunozro@est.utn.ac.cr>
2024-10-23 14:21:12 -0600

    Se actualizo la clase Cliente con sus atributos y metodos

--- Modelo/Cliente.java
+++ Modelo/Cliente.java
@@ -9,5 +9,42 @@ package Modelo;
  * @author Student
  */
 public class Cliente {
+    private String id;
+    private String nombre;
+    private int telefono;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Cliente(String id, String nombre, int telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public Cliente() {
+        this("","",0);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", telefono=" + telefono + '}';
+    }
     
+    
+    
+            
 }
